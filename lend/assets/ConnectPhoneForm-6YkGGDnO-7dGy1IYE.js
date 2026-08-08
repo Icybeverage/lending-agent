d1cffb214f58b96cd108f58cebc421152ead3760
Icybeverage <86324519+Icybeverage@users.noreply.github.com>
2026-08-08 05:11:21 +0000

    publish Sui delivery asset

---
+++ lend/assets/ConnectPhoneForm-6YkGGDnO-7dGy1IYE.js
@@ -0,0 +1,97 @@
+import{o as e}from"./rolldown-runtime-C_s2cVnS.js";import{n as t,t as n}from"./jsx-runtime-CMaQg7dW.js";import{T as r,Xt as i,Yt as a,Zt as o,an as s,c,in as l,rn as u,yn as d}from"./useActiveWallet-CEs8euQ3-CcTJ-YJ7.js";import{k as f}from"./context-Cdw68BC2-DVQ-VHhn.js";import{i as p,l as m}from"./ModalHeader-BS54PZSj-6kmwPEUZ.js";import{t as h}from"./Chip-D2-wZOHJ-CYseJ9JG.js";var g=n(),_=e(t(),1),v=({value:e,onChange:t})=>(0,g.jsx)(`select`,{value:e,onChange:t,children:l.map((e=>(0,g.jsxs)(`option`,{value:e.code,children:[e.code,` +`,e.callCode]},e.code)))}),y=(0,_.forwardRef)(((e,t)=>{let n=f(),[r,l]=(0,_.useState)(!1),{accountType:y}=c(),[S,C]=(0,_.useState)(``),[w,T]=(0,_.useState)(e.defaultCountry??n?.intl.defaultCountry??`US`),E=a(S,w),D=i(w),O=u(w),k=d(w),A=!E,[j,M]=(0,_.useState)(!1),N=k.length,P=t=>{let n=t.target.value;T(n),C(``),e.onChange&&e.onChange({rawPhoneNumber:S,qualifiedPhoneNumber:o(S,n),countryCode:n,isValid:a(S,w)})},F=(t,n)=>{try{let r=t.replace(/\D/g,``)===S.replace(/\D/g,``)?t:D.input(t);C(r),e.onChange&&e.onChange({rawPhoneNumber:r,qualifiedPhoneNumber:o(t,n),countryCode:n,isValid:a(t,n)})}catch(e){console.error(`Error processing phone number:`,e)}},I=()=>{M(!0);let t=o(S,w);e.onSubmit({rawPhoneNumber:S,qualifiedPhoneNumber:t,countryCode:w,isValid:a(S,w)}).finally((()=>M(!1)))};return(0,_.useEffect)((()=>{if(e.defaultValue){let t=s(e.defaultValue);D.reset(),P({target:{value:t.countryCode}}),F(t.phone,t.countryCode)}}),[e.defaultValue]),(0,g.jsxs)(g.Fragment,{children:[(0,g.jsx)(b,{children:(0,g.jsxs)(x,{$callingCodeLength:N,$stacked:e.stacked,children:[(0,g.jsx)(v,{value:w,onChange:P}),(0,g.jsx)(`input`,{ref:t,id:`phone-number-input`,className:`login-method-button`,type:`tel`,placeholder:O,onFocus:()=>l(!0),onChange:e=>{F(e.target.value,w)},onKeyUp:e=>{e.key===`Enter`&&I()},value:S,autoComplete:`tel`}),y!==`phone`||r||e.hideRecent?e.stacked||e.noIncludeSubmitButton?(0,g.jsx)(`span`,{}):(0,g.jsx)(p,{isSubmitting:j,onClick:I,disabled:A,children:`Submit`}):(0,g.jsx)(h,{color:`gray`,children:`Recent`})]})}),e.stacked&&!e.noIncludeSubmitButton?(0,g.jsx)(m,{loading:j,loadingText:null,onClick:I,disabled:A,children:`Submit`}):null]})})),b=r.div`
+  width: 100%;
+`,x=r.label`
+  --country-code-dropdown-width: calc(54px + calc(12 * ${e=>e.$callingCodeLength}px));
+  --phone-input-extra-padding-left: calc(12px + calc(3 * ${e=>e.$callingCodeLength}px));
+  display: block;
+  position: relative;
+  width: 100%;
+
+  /* Tablet and Up */
+  @media (min-width: 441px) {
+    --country-code-dropdown-width: calc(52px + calc(10 * ${e=>e.$callingCodeLength}px));
+  }
+
+  && > select {
+    font-size: 16px;
+    height: 24px;
+    position: absolute;
+    margin: 13px calc(var(--country-code-dropdown-width) / 4);
+    line-height: 24px;
+    width: var(--country-code-dropdown-width);
+    background-color: var(--privy-color-background);
+    background-size: auto;
+    background-position-x: right;
+    cursor: pointer;
+
+    /* Tablet and Up */
+    @media (min-width: 441px) {
+      font-size: 14px;
+      width: var(--country-code-dropdown-width);
+    }
+
+    :focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  && > input {
+    font-size: 16px;
+    line-height: 24px;
+    color: var(--privy-color-foreground);
+
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    width: calc(100% - var(--country-code-dropdown-width));
+
+    padding: 12px 88px 12px
+      calc(var(--country-code-dropdown-width) + var(--phone-input-extra-padding-left));
+    padding-right: ${e=>e.$stacked?`16px`:`88px`};
+    flex-grow: 1;
+    background: var(--privy-color-background);
+    border: 1px solid var(--privy-color-foreground-4);
+    border-radius: var(--privy-border-radius-md);
+    width: 100%;
+
+    :focus {
+      outline: none;
+      border-color: var(--privy-color-accent);
+    }
+
+    :autofill,
+    :-webkit-autofill {
+      background: var(--privy-color-background);
+    }
+
+    /* Tablet and Up */
+    @media (min-width: 441px) {
+      font-size: 14px;
+      padding-right: 78px;
+    }
+  }
+
+  && > :last-child {
+    right: 16px;
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+  }
+
+  && > button:last-child {
+    right: 0px;
+    line-height: 24px;
+    padding: 13px 17px;
+
+    :focus {
+      outline: none;
+      border-color: var(--privy-color-accent);
+    }
+  }
+
+  && > input::placeholder {
+    color: var(--privy-color-foreground-3);
+  }
+`;export{y as t};
---
+++ lend/assets/Title-BnzYV3Is-DWMRfWSU.js
@@ -0,0 +1,7 @@
+import{T as e}from"./useActiveWallet-CEs8euQ3-CcTJ-YJ7.js";var t=e.span`
+  color: var(--privy-color-foreground);
+  font-size: 1.125rem;
+  font-weight: 600;
+  line-height: 1.875rem; /* 166.667% */
+  text-align: center;
+`;export{t};
